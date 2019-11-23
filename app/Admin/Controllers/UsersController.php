de391b938a4fd512f96035b257c1327984e9e174
vagrant <vagrant@homestead>
2019-11-23 09:46:39 +0000

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('Id');
+        $grid->name('用户名');
+        $grid->phone('手机');
+        $grid->email('邮箱');
+        $grid->email_verified_at('已验证邮箱')->display(function ($value){
+            return $value?'是':'否';
+        });
+        $grid->password('密码');
+        $grid->weixin_openid('Weixin openid');
+        $grid->weixin_unionid('Weixin unionid');
+        $grid->remember_token('Remember token');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->phone('Phone');
+        $show->email('Email');
+        $show->email_verified_at('Email verified at');
+        $show->password('Password');
+        $show->weixin_openid('Weixin openid');
+        $show->weixin_unionid('Weixin unionid');
+        $show->remember_token('Remember token');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User);
+
+        $form->text('name', 'Name');
+        $form->mobile('phone', 'Phone');
+        $form->email('email', 'Email');
+        $form->datetime('email_verified_at', 'Email verified at')->default(date('Y-m-d H:i:s'));
+        $form->password('password', 'Password');
+        $form->text('weixin_openid', 'Weixin openid');
+        $form->text('weixin_unionid', 'Weixin unionid');
+        $form->text('remember_token', 'Remember token');
+
+        return $form;
+    }
+}
--- config/admin.php
+++ config/admin.php
@@ -1,317 +1,186 @@
 <?php
-
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of laravel-admin, This setting is displayed on the
-    | login page.
-    |
-    */
-    'name' => 'Laravel-admin',
+/*
+* 站点标题
+*/
+'name' => 'Laravel Shop',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin logo
-    |--------------------------------------------------------------------------
-    |
-    | The logo of all admin pages. You can also set it as an image by using a
-    | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
-    |
-    */
-    'logo' => '<b>Laravel</b> admin',
+/*
+* 页面顶部 Logo
+*/
+'logo' => '<b>Laravel</b> Shop',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin mini logo
-    |--------------------------------------------------------------------------
-    |
-    | The logo of all admin pages when the sidebar menu is collapsed. You can
-    | also set it as an image by using a `img` tag, eg
-    | '<img src="http://logo-url" alt="Admin logo">'.
-    |
-    */
-    'logo-mini' => '<b>La</b>',
+/*
+* 页面顶部小 Logo
+*/
+'logo-mini' => '<b>LS</b>',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin route settings
-    |--------------------------------------------------------------------------
-    |
-    | The routing configuration of the admin page, including the path prefix,
-    | the controller namespace, and the default middleware. If you want to
-    | access through the root path, just set the prefix to empty string.
-    |
-    */
-    'route' => [
+/*
+* 路由配置
+*/
+'route' => [
+// 路由前缀
+'prefix' => 'admin',
+// 控制器命名空间前缀
+'namespace' => 'App\\Admin\\Controllers',
+// 默认中间件列表
+'middleware' => ['web', 'admin'],
+],
 
-        'prefix' => 'admin',
+/*
+* Laravel-Admin 的安装目录
+*/
+'directory' => app_path('Admin'),
 
-        'namespace' => 'App\\Admin\\Controllers',
+/*
+* Laravel-Admin 页面标题
+*/
+'title' => 'Laravel Shop 管理后台',
 
-        'middleware' => ['web', 'admin'],
-    ],
+/*
+* 是否使用 https
+*/
+'secure' => env('ADMIN_HTTPS', false),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin install directory
-    |--------------------------------------------------------------------------
-    |
-    | The installation directory of the controller and routing configuration
-    | files of the administration page. The default is `app/Admin`, which must
-    | be set before running `artisan admin::install` to take effect.
-    |
-    */
-    'directory' => app_path('Admin'),
+/*
+* Laravel-Admin 用户认证设置
+*/
+'auth' => [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin html title
-    |--------------------------------------------------------------------------
-    |
-    | Html title for all pages.
-    |
-    */
-    'title' => 'Admin',
+'controller' => App\Admin\Controllers\AuthController::class,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Access via `https`
-    |--------------------------------------------------------------------------
-    |
-    | If your page is going to be accessed via https, set it to `true`.
-    |
-    */
-    'https' => env('ADMIN_HTTPS', false),
+'guards' => [
+'admin' => [
+'driver'   => 'session',
+'provider' => 'admin',
+],
+],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin auth setting
-    |--------------------------------------------------------------------------
-    |
-    | Authentication settings for all admin pages. Include an authentication
-    | guard and a user provider setting of authentication driver.
-    |
-    | You can specify a controller for `login` `logout` and other auth routes.
-    |
-    */
-    'auth' => [
+'providers' => [
+'admin' => [
+'driver' => 'eloquent',
+'model'  => Encore\Admin\Auth\Database\Administrator::class,
+],
+],
+],
 
-        'controller' => App\Admin\Controllers\AuthController::class,
+/*
+* Laravel-Admin 文件上传设置
+*/
+'upload' => [
+// 对应 filesystem.php 中的 disks
+'disk' => 'public',
 
-        'guards' => [
-            'admin' => [
-                'driver'   => 'session',
-                'provider' => 'admin',
-            ],
-        ],
+'directory' => [
+'image' => 'images',
+'file'  => 'files',
+],
+],
 
-        'providers' => [
-            'admin' => [
-                'driver' => 'eloquent',
-                'model'  => Encore\Admin\Auth\Database\Administrator::class,
-            ],
-        ],
-    ],
+/*
+* Laravel-Admin 数据库设置
+*/
+'database' => [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin upload setting
-    |--------------------------------------------------------------------------
-    |
-    | File system configuration for form upload files and images, including
-    | disk and upload path.
-    |
-    */
-    'upload' => [
+// 数据库连接名称，留空即可
+'connection' => '',
 
-        // Disk in `config/filesystem.php`.
-        'disk' => 'admin',
+// 管理员用户表及模型
+'users_table' => 'admin_users',
+'users_model' => Encore\Admin\Auth\Database\Administrator::class,
 
-        // Image and file upload path under the disk above.
-        'directory' => [
-            'image' => 'images',
-            'file'  => 'files',
-        ],
-    ],
+// 角色表及模型
+'roles_table' => 'admin_roles',
+'roles_model' => Encore\Admin\Auth\Database\Role::class,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel-admin database settings
-    |--------------------------------------------------------------------------
-    |
-    | Here are database settings for laravel-admin builtin model & tables.
-    |
-    */
-    'database' => [
+// 权限表及模型
+'permissions_table' => 'admin_permissions',
+'permissions_model' => Encore\Admin\Auth\Database\Permission::class,
 
-        // Database connection for following tables.
-        'connection' => '',
+// 菜单表及模型
+'menu_table' => 'admin_menu',
+'menu_model' => Encore\Admin\Auth\Database\Menu::class,
 
-        // User tables and model.
-        'users_table' => 'admin_users',
-        'users_model' => Encore\Admin\Auth\Database\Administrator::class,
+// 多对多关联中间表
+'operation_log_table'    => 'admin_operation_log',
+'user_permissions_table' => 'admin_user_permissions',
+'role_users_table'       => 'admin_role_users',
+'role_permissions_table' => 'admin_role_permissions',
+'role_menu_table'        => 'admin_role_menu',
+],
 
-        // Role table and model.
-        'roles_table' => 'admin_roles',
-        'roles_model' => Encore\Admin\Auth\Database\Role::class,
+/*
+* Laravel-Admin 操作日志设置
+*/
+'operation_log' => [
 
-        // Permission table and model.
-        'permissions_table' => 'admin_permissions',
-        'permissions_model' => Encore\Admin\Auth\Database\Permission::class,
+'enable' => true,
 
-        // Menu table and model.
-        'menu_table' => 'admin_menu',
-        'menu_model' => Encore\Admin\Auth\Database\Menu::class,
+/*
+* 不记操作日志的路由
+*/
+'except' => [
+'admin/auth/logs*',
+],
+],
 
-        // Pivot table for table above.
-        'operation_log_table'    => 'admin_operation_log',
-        'user_permissions_table' => 'admin_user_permissions',
-        'role_users_table'       => 'admin_role_users',
-        'role_permissions_table' => 'admin_role_permissions',
-        'role_menu_table'        => 'admin_role_menu',
-    ],
+/*
+* 地图组件提供商
+*/
+'map_provider' => 'google',
 
-    /*
-    |--------------------------------------------------------------------------
-    | User operation log setting
-    |--------------------------------------------------------------------------
-    |
-    | By setting this option to open or close operation log in laravel-admin.
-    |
-    */
-    'operation_log' => [
+/*
+* 页面风格
+* @see https://adminlte.io/docs/2.4/layout
+*/
+'skin' => 'skin-blue-light',
 
-        'enable' => true,
+/*
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+*/
+'layout' => ['sidebar-mini', 'sidebar-collapse'],
 
-        /*
-         * Only logging allowed methods in the list
-         */
-        'allowed_methods' => ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'],
+/*
+* 登录页背景图
+*/
+'login_background_image' => '',
 
-        /*
-         * Routes that will not log to database.
-         *
-         * All method to path like: admin/auth/logs
-         * or specific method to path like: get:admin/auth/logs.
-         */
-        'except' => [
-            'admin/auth/logs*',
-        ],
-    ],
+/*
+* 显示版本
+*/
+'show_version' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Admin map field provider
-    |--------------------------------------------------------------------------
-    |
-    | Supported: "tencent", "google", "yandex".
-    |
-    */
-    'map_provider' => 'google',
+/*
+* 显示环境
+*/
+'show_environment' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Skin
-    |--------------------------------------------------------------------------
-    |
-    | This value is the skin of admin pages.
-    | @see https://adminlte.io/docs/2.4/layout
-    |
-    | Supported:
-    |    "skin-blue", "skin-blue-light", "skin-yellow", "skin-yellow-light",
-    |    "skin-green", "skin-green-light", "skin-purple", "skin-purple-light",
-    |    "skin-red", "skin-red-light", "skin-black", "skin-black-light".
-    |
-    */
-    'skin' => 'skin-blue-light',
+/*
+* 菜单绑定权限
+*/
+'menu_bind_permission' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application layout
-    |--------------------------------------------------------------------------
-    |
-    | This value is the layout of admin pages.
-    | @see https://adminlte.io/docs/2.4/layout
-    |
-    | Supported: "fixed", "layout-boxed", "layout-top-nav", "sidebar-collapse",
-    | "sidebar-mini".
-    |
-    */
-    'layout' => ['sidebar-mini', 'sidebar-collapse'],
+/*
+* 默认启用面包屑
+*/
+'enable_default_breadcrumb' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Login page background image
-    |--------------------------------------------------------------------------
-    |
-    | This value is used to set the background image of login page.
-    |
-    */
-    'login_background_image' => '',
+/*
+* 扩展所在的目录.
+*/
+'extension_dir' => app_path('Admin/Extensions'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Show version at footer
-    |--------------------------------------------------------------------------
-    |
-    | Whether to display the version number of laravel-admim at the footer of
-    | each page
-    |
-    */
-    'show_version' => true,
+/*
+* 扩展设置.
+*/
+'extensions' => [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Show environment at footer
-    |--------------------------------------------------------------------------
-    |
-    | Whether to display the environment at the footer of each page
-    |
-    */
-    'show_environment' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Menu bind to permission
-    |--------------------------------------------------------------------------
-    |
-    | whether enable menu bind to a permission
-    */
-    'menu_bind_permission' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Enable default breadcrumb
-    |--------------------------------------------------------------------------
-    |
-    | Whether enable default breadcrumb for every page content.
-    */
-    'enable_default_breadcrumb' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Extension Directory
-    |--------------------------------------------------------------------------
-    |
-    | When you use command `php artisan admin:extend` to generate extensions,
-    | the extension files will be generated in this directory.
-    */
-    'extension_dir' => app_path('Admin/Extensions'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Settings for extensions.
-    |--------------------------------------------------------------------------
-    |
-    | You can find all available extensions here
-    | https://github.com/laravel-admin-extensions.
-    |
-    */
-    'extensions' => [
-
-    ],
+],
 ];
